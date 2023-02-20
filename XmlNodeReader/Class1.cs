5286768f3c383ec2325679e1a056dabc0aaec8e4
LucasFransson <113167169+LucasFransson@users.noreply.github.com>
2023-02-20 01:32:30 +0100

    Initial commit

---
+++ XmlNodeReader/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml;
+
+namespace Xml
+{
+    public class XmlNodeReader
+    {
+        public string GetNodeInnerTextFromString(string xmlString, string stringTargetNode)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlString);
+
+            XmlNode targetNode = doc.SelectSingleNode(stringTargetNode);
+
+            return targetNode.InnerText;
+        }
+        public string GetNodeTextFromFile(string filePath, string stringTargetNode)
+        {
+            return GetNodeTextValue(GetXmlNode(GetXmlDocFromFile(filePath), stringTargetNode));
+        }
+
+        public string GetNodeTextValue(XmlNode node)
+        {
+            return node.InnerText;
+        }
+
+        public XmlNode GetXmlNode(XmlDocument xmlDoc, string stringTargetNode)
+        {
+            return xmlDoc.SelectSingleNode(stringTargetNode);
+        }
+
+        public XmlDocument GetXmlDocFromFile(string filePath)
+        {
+            XmlDocument document = new();
+            document.Load(filePath);
+            return document;
+        }
+    }
+}
